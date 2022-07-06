e2f0f1ef8ccad4eb402a57f8a94771f61636a660
Oluseyi Imanah <seyicole@gmail.com>
2022-07-06 10:01:33 +0100

    setiing up flask

---
+++ python_note/dictionary_2.py
@@ -0,0 +1,18 @@
+fav_songs = [{
+    "Artist":"Asa",
+    "Song Name":"Jailer",
+    "Genre":"Pop Afro",
+    "Release Year":"2005"
+},
+{
+    "Artist":"Coldplay",
+    "Song Name":"Clocks",
+    "Genre":"Alternative",
+    "Release Year":"2003"
+},
+{
+    "Artist":"Post Malone",
+    "Song Name":"On the road",
+    "Genre":"Hip hop",
+    "Release Year":"2016"
+}]
